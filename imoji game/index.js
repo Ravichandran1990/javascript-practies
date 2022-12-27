3dfc1f356d44a31574a61979b0f5a25b7aaa58d6
AtsD:\Project\angular\teamplay\Protocols.UI#45Vip$ <ravichandran.m.ext@siemens-healthineers.com>
2022-12-27 16:16:32 +0530

    emojigames

---
+++ imoji game/index.js
@@ -0,0 +1,138 @@
+
+const emojiArr = [
+    '9996',
+    '129501',
+    '129500',
+    '129498',
+    '129497',
+    '129496',
+    '129495',
+    '129494',
+    '129493',
+    '129492',
+    '129491',
+    '129490',
+    '129491',
+    '129490',
+    '129489',
+    '129342',
+    '129341',
+    '129337',
+    '129336',
+    '129335',
+    '129334',
+    '129333',
+    '129332',
+    '129331',
+    '129330',
+]
+
+const row = 4;
+const column = 4;
+const min = 0;
+const max = emojiArr.length - 1;
+let maxattempts = 0;
+let setMaxAttempts = 35;
+const attempts = document.querySelector('.attempts');
+const emojiContainer = document.querySelector('.emojiContainer');
+for (let i = 0; i < row; i++) {
+     for (let j = 0; j < column; j++) {
+        const div1 = document.createElement("div");        
+        div1.setAttribute('data-i', i);
+        div1.setAttribute('data-j', j);
+        (i+j)%2 === 0 ? div1.classList.add('even') : div1.classList.add('odd');        
+        emojiContainer.appendChild(div1);
+        setListener( div1 );
+     }    
+}
+
+const emojiContainerDiv = document.querySelectorAll('.emojiContainer div');
+let emojiIndexedArray;
+emojiSetData();
+function emojiSetData() {
+    emojiIndexedArray = [];
+    for (let index = 0; index < emojiContainerDiv.length - 8; index++) {
+        const index = getRandomBoxElem();
+        emojiIndexedArray.push(index);
+        emojiIndexedArray.push(emojiContainerDiv.length - index);
+        const emojiNo = "&#"+getRandomEmojiNo();
+        spanEmojiEle(emojiContainerDiv[index], emojiNo);  
+        spanEmojiEle(emojiContainerDiv[(emojiContainerDiv.length - 1) - index], emojiNo);  
+    }
+}
+
+function spanEmojiEle(ele, emoji) {
+    const span = document.createElement("span");
+    span.setAttribute('data-emoji', emoji);
+    ele.appendChild(span);
+}
+
+function getRandomBoxElem() {
+    const index = Math.round(Math.random() * (7 - 0)) + 0;
+    return emojiIndexedArray.indexOf(index) === -1 ? index : getRandomBoxElem();
+}
+function getRandomEmojiNo() {
+    const index = Math.round(Math.random() * (max - min)) + min;
+    return emojiArr[index] === 'undefined' ? getRandomEmojiNo() : emojiArr[index];
+}
+
+let clickCount = 0;
+function setListener( el ) {
+    // Dynamic elements event listener
+    el.addEventListener( 'click', function(event) {
+        const objectVal = Object.values(event.target.classList);
+        if(objectVal.indexOf('selected') === -1 && objectVal.indexOf('matched') === -1 && event.target.innerText === '') {
+            randomEmojiSetter(event.target);
+            maxattempts++;
+            attempts.querySelector('span').innerHTML = maxattempts;
+            reachedMaxAttempts()
+        }        
+    });
+}
+
+
+function randomEmojiSetter(event) {
+    clickCount++;
+    event.classList.add('selected');
+    const spanEle = event.querySelector('span');
+    const emoji = spanEle.getAttribute('data-emoji');
+    spanEle.innerHTML = emoji;
+
+    if(clickCount >= 2) {
+        clearEmojiData();
+        clickCount = 0;
+    }
+    
+}
+let timer;
+function clearEmojiData() {
+    const emojiData = document.querySelectorAll('.emojiContainer div.selected');
+    const selected1 = emojiData[0].querySelector('span').innerHTML;
+    const selected2 = emojiData[1].querySelector('span').innerHTML;
+    if(selected1 === selected2) {
+        emojiData[0].classList.add('matched');
+        emojiData[1].classList.add('matched');
+    }
+    for (let index = 0; index < emojiData.length; index++) {         
+        emojiData[index].classList.remove('selected');
+        if(selected1 !== selected2) {
+            removeSelector(emojiData[index]);            
+        }      
+    }
+}
+
+function removeSelector(event) {       
+    timer = setTimeout(() => {
+        event.querySelector('span').innerHTML = "";
+    }, 1000);   
+}
+
+function reachedMaxAttempts() {
+    if(maxattempts > setMaxAttempts) {        
+        document.querySelector('.maxattempts').innerHTML = "You reached maximum attempts. You loose the game. try to to re-attempt."
+        setTimeout(() => {
+            document.querySelector('.maxattempts').innerHTML = "";
+            window.location.reload('/');
+        }, 2000)
+    }   
+}
