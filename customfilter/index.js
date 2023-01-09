e60268dc9d8e4f68889f7185ba2ed7dc46421f24
AtsD:\Project\angular\teamplay\Protocols.UI#45Vip$ <ravichandran.m.ext@siemens-healthineers.com>
2023-01-09 19:06:07 +0530

    custom filter javascript

---
+++ customfilter/index.js
@@ -0,0 +1,42 @@
+const data = [1, 2, 3, 4, 5];
+
+// Array.prototype.myCustomFilter = function (fn) {
+//   const filtered = []; // it will receive all values that match to condition passed in fn callback.
+
+//   for (let i = 0; i < this.length; i++) {
+//     console.log(fn(this[i]));
+//     if (fn(this[i])) {
+//       filtered.push(this[i]);
+//     }
+//   }
+
+//   return filtered;
+// };
+
+// const filteredData = data.myCustomFilter(myFunction);
+// function myFunction(el) {
+//     if (el > 2) return el;
+// }
+// console.log(filteredData);
+
+
+Array.prototype.myCustomMap = function (fn) {
+  const filtered = []; // it will receive all values that match to condition passed in fn callback.
+
+  for (let i = 0; i < this.length; i++) {
+    console.log(fn(this[i]));
+    if (fn(this[i])) {
+      filtered.push(fn(this[i]));
+    }else {
+        filtered.push(this[i]); 
+    }
+  }
+
+  return filtered;
+};
+
+const filteredData = data.myCustomMap(myFunction);
+function myFunction(el) {
+    if (el > 2) return el * 2;
+}
+console.log(filteredData);
